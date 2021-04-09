f359a89a8e84aaa3842deecf128cd47635bd0bf9
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-04-09 12:04:04 -0500

    Completando tests para <GifGrid />
    Implementando mocks para simular el funcionamiento del custom hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useFetchGifs from '../hooks/useFetchGifs';
+import PropTypes from 'prop-types'
 import { GifGridItem } from './GifGridItem';
 import { Loader } from './Loader';
 
@@ -21,3 +22,7 @@ export const GifGrid = ({ category }) => {
     </>
   );
 };
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+}
